cd6ae41ebdf7516ff79493898aa24d2f53cf9875
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-07-02 19:47:19 +0100

    modified

--- src/js/events.js
+++ src/js/events.js
@@ -1,32 +1,32 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
-// Init Swiper
-const swiperEvents = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
-    loop: false,
-    spaceBetween: 20,
-    slidesPerView: 1,
-    navigation: {
-        nextEl: ".custom-next",
-        prevEl: ".custom-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
-            centeredSlides: true,
-        },
-        1024: {
-            slidesPerView: 3,
-            centeredSlides: false,
-        },
-    },
+// // Init Swiper
+// const swiperEvents = new Swiper('.swiper', {
+//     modules: [Navigation, Pagination],
+//     loop: false,
+//     spaceBetween: 20,
+//     slidesPerView: 1,
+//     navigation: {
+//         nextEl: ".custom-next",
+//         prevEl: ".custom-prev",
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     breakpoints: {
+//         768: {
+//             slidesPerView: 2,
+//             centeredSlides: true,
+//         },
+//         1024: {
+//             slidesPerView: 3,
+//             centeredSlides: false,
+//         },
+//     },
 
-})
+// })
