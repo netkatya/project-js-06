2ba00a0f0fa61269497f5919f3cbb8d1628701bc
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-07-03 21:10:49 +0100

    modified

---
+++ src/js/modal.js
@@ -0,0 +1,6 @@
+const eventModal = document.querySelector(".modal-overlay")
+const closeButton = document.querySelector(".events-close-button");
+
+closeButton.addEventListener("click", () => {
+    eventModal
+})
