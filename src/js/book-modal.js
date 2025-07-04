4083557a93a84659adfe620656d907f24bc9b1f6
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-07-04 23:26:27 +0100

    modal-modif

--- src/js/book-modal.js
+++ src/js/book-modal.js
@@ -1,3 +1,26 @@
 import Accordion from "accordion-js";
 import "accordion-js/dist/accordion.min.css";
 
+
+const accordionTriggers = document.querySelectorAll(".ac-trigger");
+
+accordionTriggers.forEach(trigger => {
+  trigger.addEventListener("click", () => {
+    const accordionItem = trigger.closest(".ac");
+    const panel = accordionItem.querySelector(".ac-panel");
+    const isActive = accordionItem.classList.contains("is-active");
+
+
+    document.querySelectorAll(".ac").forEach(item => {
+      item.classList.remove("is-active");
+      const p = item.querySelector(".ac-panel");
+      p.style.maxHeight = null;
+    });
+
+   
+    if (!isActive) {
+      accordionItem.classList.add("is-active");
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+});
