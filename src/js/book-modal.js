583ddb564383974a69c83d105a57d830e3e35130
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-07-06 16:49:10 +0100

    modif loader

--- src/js/book-modal.js
+++ src/js/book-modal.js
@@ -2,8 +2,6 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import { booksRefs, showInfoMsg } from './helpers';
 
-console.dir(Accordion);
-
 // new Accordion('.accordion-container', {
 //   duration: 500,
 //   showMultiple: true,
