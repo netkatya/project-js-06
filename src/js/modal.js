48a4ec2897b6796c2ff315fe1b5f2c10defb9bc1
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-07-07 18:24:34 +0100

    modif

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,21 +1,26 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const eventModal = document.querySelector('.events-modal-overlay-events');
 const closeButton = document.querySelector(
   '.events-modal-overlay-events .events-close-button'
 );
 const modalTitle = document.querySelector('.events-modal-text');
+const registerForm = document.querySelector('.modal-form'); // Форма
+
 
 let scrollTop = 0;
+let currentEventTitle = '';
 
 function openModal(eventName) {
   scrollTop = window.scrollY;
-
+  currentEventTitle = eventName;
   modalTitle.textContent = eventName;
 
   eventModal.classList.add('is-open');
-  document.body.classList.add('modal-open'); // scroll blocked
+  document.body.classList.add('modal-open');
   document.documentElement.classList.add('modal-open');
   document.body.style.top = `-${scrollTop}px`;
-  // document.body.style.position = 'fixed';
   document.body.style.width = '100%';
 }
 
@@ -23,7 +28,6 @@ function closeModal() {
   eventModal.classList.remove('is-open');
   document.body.classList.remove('modal-open');
   document.documentElement.classList.remove('modal-open');
-
   document.body.style.position = '';
   document.body.style.width = '';
 
@@ -32,14 +36,14 @@ function closeModal() {
   window.scrollTo(0, scrollY);
 }
 
-// Закриття по кліку на backdrop
+// Закрытие по клику на фон
 eventModal.addEventListener('click', event => {
   if (event.target === eventModal) {
     closeModal();
   }
 });
 
-// Закриття по натисканню Escape
+// esc
 window.addEventListener('keydown', event => {
   if (event.key === 'Escape') {
     event.preventDefault();
@@ -49,6 +53,7 @@ window.addEventListener('keydown', event => {
 
 closeButton.addEventListener('click', closeModal);
 
+
 const registerButtons = document.querySelectorAll('.events-list-button');
 
 registerButtons.forEach(button => {
@@ -57,4 +62,34 @@ registerButtons.forEach(button => {
     const title = card.querySelector('.events-list-header').textContent.trim();
     openModal(title);
   });
+});
+
+
+registerForm.addEventListener('submit', event => {
+  event.preventDefault(); 
+
+  const name = registerForm.querySelector('input[name="events-name"]').value.trim();
+  const email = registerForm.querySelector('input[name="events-email"]').value.trim();
+
+  if (!name || !email) {
+    iziToast.error({
+      title: 'Missing Fields',
+      message: 'Please fill in both your name and email.',
+      position: 'topRight',
+      timeout: 3000,
+    });
+    return;
+  }
+
+
+  iziToast.success({
+    title: 'Success',
+    message: `You have successfully registered for the event: "${currentEventTitle}"`,
+    position: 'topRight',
+    timeout: 3000,
+
+  });
+
+  registerForm.reset(); 
+  closeModal();
 });
